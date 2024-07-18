6906534059d226224bf43bbec79dc2dcbd7458cf
Your Name <you@example.com>
2024-07-18 19:37:45 -0400

    Initial commit

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -13,7 +13,7 @@ const Hero = () => {
         <div className="hero-action">
             <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with Me</AnchorLink></div>
             <div className="hero-resume">
-                <a href='Thapaswini_resume.pdf' target='_blank' rel='noopener noreferrer'>My Resume</a>
+                <a href='Resume.pdf' target='_blank' rel='noopener noreferrer'>My Resume</a>
             </div>
         </div>
       
